bf9e40d81d19a9b0cc7e11304345bf67835e6bc5
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-10-02 03:38:10 +0300

    Updates

---
+++ precache-manifest.1a2a4947d4d55d048d75d65b5b2f2662.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acaaa089d8b77bd416ae31daeb741cde",
+    "url": "/spa-react-router/index.html"
+  },
+  {
+    "revision": "e8d31a745f44b83687d2",
+    "url": "/spa-react-router/static/css/main.9bac6365.chunk.css"
+  },
+  {
+    "revision": "06f95a63eada4cb7bd85",
+    "url": "/spa-react-router/static/js/2.daa60877.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spa-react-router/static/js/2.daa60877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d31a745f44b83687d2",
+    "url": "/spa-react-router/static/js/main.adaa7bdc.chunk.js"
+  },
+  {
+    "revision": "b7e78ec4677d95936a82",
+    "url": "/spa-react-router/static/js/runtime-main.540a5d85.js"
+  },
+  {
+    "revision": "99243e5afd8fc82f32f8bc1780879f43",
+    "url": "/spa-react-router/static/media/1251845.99243e5a.svg"
+  },
+  {
+    "revision": "c6972b988fe0baf17c4269bb5494986a",
+    "url": "/spa-react-router/static/media/134932.c6972b98.svg"
+  },
+  {
+    "revision": "f5766427d718271a13afed7650738fa7",
+    "url": "/spa-react-router/static/media/454545.f5766427.svg"
+  },
+  {
+    "revision": "01b91a1bdd82e18961f3bc2f42918fc3",
+    "url": "/spa-react-router/static/media/929417.01b91a1b.svg"
+  },
+  {
+    "revision": "e10b52fc8d6e3059135c0b43c1ea5b60",
+    "url": "/spa-react-router/static/media/boy.e10b52fc.jpg"
+  },
+  {
+    "revision": "e99b4caf9538f61f18a508bc95739860",
+    "url": "/spa-react-router/static/media/cat.e99b4caf.jpeg"
+  },
+  {
+    "revision": "74e5df08380297e5b47c64585193dde2",
+    "url": "/spa-react-router/static/media/code.74e5df08.jpeg"
+  },
+  {
+    "revision": "f916177f979c36d7d54332a737ac1231",
+    "url": "/spa-react-router/static/media/girl.f916177f.jpg"
+  },
+  {
+    "revision": "a2693329ba11b5b4b58e2b1e0fff5bae",
+    "url": "/spa-react-router/static/media/iland.a2693329.jpeg"
+  },
+  {
+    "revision": "75abf4cfaeb7c3f590bcac5fe2e2a014",
+    "url": "/spa-react-router/static/media/photo1.75abf4cf.jpeg"
+  },
+  {
+    "revision": "3161130443bae7fe16f013be95b486d7",
+    "url": "/spa-react-router/static/media/photo2.31611304.jpeg"
+  },
+  {
+    "revision": "a14ee555da6647f7ec0530fc91351314",
+    "url": "/spa-react-router/static/media/photo3.a14ee555.jpeg"
+  },
+  {
+    "revision": "2c4e768a0367076780707fa0885ebcc7",
+    "url": "/spa-react-router/static/media/photo5.2c4e768a.jpeg"
+  },
+  {
+    "revision": "c62a448720d8064a619d2db18daa4221",
+    "url": "/spa-react-router/static/media/sea.c62a4487.jpeg"
+  },
+  {
+    "revision": "97b0741c3ace988ea6f81def1517cf96",
+    "url": "/spa-react-router/static/media/serfing.97b0741c.jpeg"
+  }
+]);
